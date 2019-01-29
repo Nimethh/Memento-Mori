4c786adb98103ce79d6f8ead96f8780642424fc4
Migge <miggex91@hotmail.com>
2019-01-30 00:40:02 +0100

    Added cursor, scene changes
    Added an intro scene
    Added cursor
    Added animations
    Added a gun that follows the cursor

---
+++ Assets/Scripts/TestArmRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestArmRotation : MonoBehaviour
+{
+    public int rotationOffset = 0;
+
+    void Update()
+    {
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg; //Finding the angle in degrees
+        transform.rotation = Quaternion.Euler(0f, 0f, rotationZ + rotationOffset);
+    }
+}
---
+++ Assets/Scripts/TestCursor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCursor : MonoBehaviour
+{
+    private SpriteRenderer renderer;
+    [SerializeField]
+    public Sprite cursorBlue;
+    [SerializeField]
+    public Sprite cursorRed;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            renderer.sprite = cursorRed;
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            renderer.sprite = cursorBlue;
+        }
+
+    }
+}
