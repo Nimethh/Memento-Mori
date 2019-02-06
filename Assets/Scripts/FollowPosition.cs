72c6ddc927630245ccad5406dc0fec671c203dc7
Karam Hariri <karam.hariri@outlook.com>
2019-02-06 15:42:21 +0100

    Merged enemies and movement to the scene 1

--- Assets/Scripts/FollowPosition.cs
+++ Assets/Scripts/FollowPosition.cs
@@ -5,24 +5,16 @@ using UnityEngine;
 public class FollowPosition : MonoBehaviour
 {
     public float speed;
-
-    //private float moveHorizontal;
-    //Rigidbody rb;
-
+    
     private Transform player;
 
     void Start()
     {
-    //    rb = GetComponent<Rigidbody>();
         player = GameObject.FindGameObjectWithTag("PlayerCube").transform;
-        //moveHorizontal = -1;
     }
 
     void Update()
     {
-        //if (transform.position.x > 8.0f)
-        //    rb.velocity = new Vector3(moveHorizontal * speed, 0.0f, 0.0f);
-        //else if(transform.position.x < 8.0f)
-            transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
 }
