2d31f75cc1e496db754cad39103b4f73a4119aac
Migge <miggex91@hotmail.com>
2019-02-24 00:30:13 +0100

    Added the color changing feature on the headupgrade
    Created a new tag for the background, the object that holds all the sprites need to have this tag and the backgroundColorChanger script.

---
+++ Assets/Scripts/BackgroundColorChanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundColorChanger : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer[] backgroundSprites;
+
+    private Color redHue;
+    private Color normal;
+
+    void Start()
+    {
+        redHue = new Color(1f, 0.16f, 0.16f, 1f);
+        normal = new Color(1, 1, 1, 1);
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ChangeHueToRed()
+    {
+        Debug.Log("Changing Hue to Red");
+        for (int i = 0; i < backgroundSprites.Length; i++)
+        {
+            backgroundSprites[i].color = redHue;
+        }
+    }
+
+    public void ChangeBackToNormal()
+    {
+        Debug.Log("Changing Hue to Normal");
+
+        for (int i = 0; i < backgroundSprites.Length; i++)
+        {
+            backgroundSprites[i].color = normal;
+        }
+    }
+
+}
