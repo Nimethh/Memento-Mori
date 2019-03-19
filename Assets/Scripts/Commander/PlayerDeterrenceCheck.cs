9127f9e9a1bde0e2f5ecd6dd23944bf7b51f2679
Migge <miggex91@hotmail.com>
2019-03-19 16:21:24 +0100

    Fixed the commander
    Added collision with the player
    Added a way to deter the player from staying below or above the commander

---
+++ Assets/Scripts/Commander/PlayerDeterrenceCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeterrenceCheck : MonoBehaviour
+{
+    private GameObject player;
+    [SerializeField]
+    private GameObject deterrenceObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("PlayerCube");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.position.x > transform.position.x)
+        {
+            deterrenceObject.SetActive(true);
+        }
+        else
+        {
+            deterrenceObject.SetActive(false);
+        }
+    }
+}
