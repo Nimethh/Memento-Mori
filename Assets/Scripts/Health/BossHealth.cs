69433b9bd482ab0e2153619906b65ae53544a5ea
James Kerber <kerber.jg@gmail.com>
2019-03-22 02:53:45 +0100

    Written a custom script for boss' health
    Takes 2 stages into account, takes progressively less damage during the second stage, until 50% of health is reached, after which the player will deal so little damage that they will have do deploy the EMP

---
+++ Assets/Scripts/Health/BossHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour, IHealth
+{
+    public float health;
+    public float secondStageHealth;
+    public bool isSecondStage = false;
+    void Update()
+    {
+        Animator anim = GetComponent<Animator>();
+        if (health <= 0)
+        {
+            //Instantiate(explosion, transform.position, Quaternion.identity);
+            //FindObjectOfType<AudioManager>().Play("MinionDestroyed");
+            //Destroy(gameObject);
+        }
+    }
+
+    public void TakeEmp() {
+        health = 0;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        this.TakeDamage((float) damage);
+    }
+
+    // During its second stage, it gets progressively harder to deal damage (with hardest starting at 50% of damage)
+    public void TakeDamage(float damage)
+    {
+        if(!isSecondStage)
+            health -= damage;
+        else {
+            float healthNormalized = health / secondStageHealth;
+            float lerpIndex = Mathf.Max((healthNormalized - 0.5f) * 2f, 0f);
+
+            health -= damage * Mathf.Lerp(0.1f, 1f, Mathf.Sqrt(lerpIndex)); // square root makes sure the progression is slower
+        }
+        
+        FindObjectOfType<AudioManager>().Play("MinionDamaged");
+    }
+
+    public void EnterSecondStage() {
+        isSecondStage = true;
+        health = secondStageHealth;
+    }
+
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
